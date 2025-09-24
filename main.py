d1c9c0e50b85f13adca5fa59af1e53a58675308f
SAINIHAR123 <sainihar32361@gmail.com>
2025-09-24 11:09:34 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+
+
+#input the name from users
+name=str(input("enter your name:"))
+age=int(input("enter the age:"))
+gender=str(input("enter the gender:"))
+
+#display
+print(f"Hello, {name} your age is {age} and your gender is {gender}")
